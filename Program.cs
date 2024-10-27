fb4cc6548d63afef10f74ebf0a462018b143f2a2
barkan33 <mishakolomyza@gmail.com>
2024-10-27 15:09:52 +0200

    Create SnakeAndLaddersGame Class

--- Program.cs
+++ Program.cs
@@ -36,6 +36,7 @@ namespace SnakeNlLaddersGame
             foreach (Structure item in snakes)
                 Console.WriteLine(item);
 
+
             SnakeAndLaddersGame game = new SnakeAndLaddersGame(boardSize, DiceSides, players, ladders, snakes, goldenCells);
             game.PlayGame();
         }
@@ -126,97 +127,15 @@ namespace SnakeNlLaddersGame
             while (goldenCells.Count < numberOfGoldenCells)
             {
                 int cell = random.Next(10, boardSize);
-                if (!goldenCells.Contains(cell) && !ladders.Any(ladder => ladder.Start == cell || ladder.End == cell) && !snakes.Any(snake => snake.Start == cell || snake.End == cell))
+                //Is cell exists
+                if (!goldenCells.Contains(cell) &&
+                    !ladders.Any(ladder => ladder.Start == cell || ladder.End == cell) &&
+                    !snakes.Any(snake => snake.Start == cell || snake.End == cell))
                 {
                     goldenCells.Add(cell);
                 }
             }
             return goldenCells;
-        }
-
-        class SnakeAndLaddersGame
-        {
-            public int BoardSize { get; set; }
-            public int DiceSides { get; set; }
-            public List<Player> Players { get; set; }
-            public List<Structure> Ladders { get; set; }
-            public List<Structure> Snakes { get; set; }
-            public List<int> GoldenCells { get; set; }
-
-            public SnakeAndLaddersGame(int boardSize, int diceSides, List<Player> players, List<Structure> ladders, List<Structure> snakes, List<int> goldenCells)
-            {
-                BoardSize = boardSize;
-                DiceSides = diceSides;
-                Players = players;
-                Ladders = ladders;
-                Snakes = snakes;
-                GoldenCells = goldenCells;
-            }
-            public void PlayGame()
-            {
-                Random random = new Random();
-                int currentPlayerIndex = 0;
-
-                IEnumerable<Structure> structures = Ladders.Concat(Snakes);
-
-                while (true)
-                {
-                    Console.WriteLine($"\nIt's {Players[currentPlayerIndex].Name}'s turn!");
-
-                    int diceRoll1 = random.Next(1, 7);
-                    int diceRoll2 = random.Next(1, 7);
-                    Console.WriteLine($"Dice rolls: {diceRoll1}, {diceRoll2}");
-
-                    Players[currentPlayerIndex].Position += diceRoll1 + diceRoll2;
-
-
-                    if (Players[currentPlayerIndex].Position > BoardSize)
-                    {
-                        Players[currentPlayerIndex].Position = BoardSize;
-                        break;
-                    }
-
-
-                    if (GoldenCells.Contains(Players[currentPlayerIndex].Position))
-                    {
-                        int leadingPlayerIndex = Players.IndexOf(Players.OrderBy(p => p.Position).Last());
-
-                        if (Players[leadingPlayerIndex] == Players[currentPlayerIndex])
-                        {
-                            leadingPlayerIndex = Players.IndexOf(Players.OrderBy(p => p.Position).First());
-                        }
-
-                        Console.WriteLine($"You landed on a Golden Cell! Switching places with {Players[leadingPlayerIndex].Name}");
-                        (Players[currentPlayerIndex].Position, Players[leadingPlayerIndex].Position) = (Players[leadingPlayerIndex].Position, Players[currentPlayerIndex].Position);
-                    }
-
-
-                    foreach (Structure structure in structures)
-                    {
-                        if (Players[currentPlayerIndex].Position == structure.Start)
-                        {
-                            if (structure.Start < structure.End)
-                            {
-                                Console.WriteLine($"Congratulations! Ladder! You climb up from cell {Players[currentPlayerIndex].Position} to cell {structure.End}");
-                                Players[currentPlayerIndex].Position = structure.End;
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Oops! Snake! You slide down from cell {Players[currentPlayerIndex].Position} to cell {structure.End}");
-                                Players[currentPlayerIndex].Position = structure.End;
-                                break;
-                            }
-                        }
-                    }
-
-
-                    Console.WriteLine($"Current position: {Players[currentPlayerIndex].Position}");
-
-                    currentPlayerIndex = (currentPlayerIndex + 1) % Players.Count;
-                }
-                Console.WriteLine($"\nCongratulations, {Players[currentPlayerIndex].Name}, you won!");
-            }
         }
 
     }
---
+++ SnakeAndLaddersGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeNlLaddersGame
+{
+    class SnakeAndLaddersGame
+    {
+        public int BoardSize { get; private set; }
+        public int DiceSides { get; private set; }
+        public List<Player> Players { get; private set; }
+        public List<Structure> Ladders { get; private set; }
+        public List<Structure> Snakes { get; private set; }
+        public List<int> GoldenCells { get; private set; }
+
+        public SnakeAndLaddersGame(int boardSize, int diceSides, List<Player> players, List<Structure> ladders, List<Structure> snakes, List<int> goldenCells)
+        {
+            BoardSize = boardSize;
+            DiceSides = diceSides;
+            Players = players;
+            Ladders = ladders;
+            Snakes = snakes;
+            GoldenCells = goldenCells;
+        }
+        public void PlayGame()
+        {
+            Random random = new Random();
+            int currentPlayerIndex = 0;
+
+            IEnumerable<Structure> structures = Ladders.Concat(Snakes);
+
+            while (true)
+            {
+                Console.WriteLine($"\nIt's {Players[currentPlayerIndex].Name}'s turn!");
+
+                int diceRoll1 = random.Next(1, 7);
+                int diceRoll2 = random.Next(1, 7);
+                Console.WriteLine($"Dice rolls: {diceRoll1}, {diceRoll2}");
+
+                Players[currentPlayerIndex].Position += diceRoll1 + diceRoll2;
+
+
+                if (Players[currentPlayerIndex].Position > BoardSize)
+                {
+                    Players[currentPlayerIndex].Position = BoardSize;
+                    break;
+                }
+
+
+                if (GoldenCells.Contains(Players[currentPlayerIndex].Position))
+                {
+                    int leadingPlayerIndex = Players.IndexOf(Players.OrderBy(p => p.Position).Last());
+
+                    if (Players[leadingPlayerIndex] == Players[currentPlayerIndex])
+                    {
+                        leadingPlayerIndex = Players.IndexOf(Players.OrderBy(p => p.Position).First());
+                    }
+
+                    Console.WriteLine($"You landed on a Golden Cell! Switching places with {Players[leadingPlayerIndex].Name}");
+                    (Players[currentPlayerIndex].Position, Players[leadingPlayerIndex].Position) = (Players[leadingPlayerIndex].Position, Players[currentPlayerIndex].Position);
+                }
+
+
+                foreach (Structure structure in structures)
+                {
+                    if (Players[currentPlayerIndex].Position == structure.Start)
+                    {
+                        //Ladder or Snake
+                        if (structure.Start < structure.End)
+                            Console.WriteLine($"Congratulations! Ladder! You climb up from cell {Players[currentPlayerIndex].Position} to cell {structure.End}");
+
+                        else
+                            Console.WriteLine($"Oops! Snake! You slide down from cell {Players[currentPlayerIndex].Position} to cell {structure.End}");
+
+                        Players[currentPlayerIndex].Position = structure.End;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"Current position: {Players[currentPlayerIndex].Position}");
+
+                currentPlayerIndex = (currentPlayerIndex + 1) % Players.Count;
+            }
+            Console.WriteLine($"\nCongratulations, {Players[currentPlayerIndex].Name}, you won!");
+        }
+    }
+}
